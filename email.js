72315ff1903156826c35005efb929c640cd85237
Boyan Gyurov <bobigurov36@gmail.com>
2024-05-22 10:47:09 +0300

    Fix some bugs

--- email.js
+++ email.js
@@ -1,26 +1,22 @@
+function sendMail(event) {
+  event.preventDefault(); 
 
-function sendMail() {
-    
-   
-    let params = {
+  let params = {
       name: document.getElementById("name").value,
       email: document.getElementById("email").value,
       message: document.getElementById("message").value,
-    };
-  
-    const serviceID = "service_4tgf33r";
-    const templateID = "template_gpb2o1o"; 
-  
-      emailjs.send(serviceID, templateID, params)
-      .then(res=>{
-          document.getElementById("name").value = "";
-          document.getElementById("email").value = "";
-          document.getElementById("message").value = "";
-          console.log(res);
-          alert("Съобщението е изпратено успешно!!")
-  
-      })
-      .catch(err=>console.log(err));
-  
-  
-  }
+  };
+
+  const serviceID = "service_4tgf33r";
+  const templateID = "template_gpb2o1o"; 
+
+  emailjs.send(serviceID, templateID, params)
+  .then(res => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      console.log(res);
+      alert("Съобщението е изпратено успешно!!");
+  })
+  .catch(err => console.log(err));
+}
